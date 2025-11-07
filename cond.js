e2123822faca3dc0c00aa47988ddce6e152d8c21
akhilmanne23-star <akhilmanne23@gmail.com>
2025-11-06 16:23:26 -0800

    second commit

---
+++ cond.js
@@ -0,0 +1,29 @@
+// Example: "I'll study at the libarary if it's open and if i dont have any chores at home."
+
+// AND
+// // Two varaibles: libarayOpen, noChores
+
+// let libraryOpen = true;
+// let noChores = true;
+
+// if (libraryOpen && !noChores) {
+
+//     console.log("i will study at the library");
+// } else{
+// console.log ("The library is closed and i have chores to do, i dont have any chores but the libaray is closed, Libaray is open but i have chores");
+// }
+
+// let budget = 50;
+// let price = 40;
+// if (budget >= price) {
+//     console.log("i will buy the item");
+// } else {
+//     console.log("i cant buy the item");
+// }
+let budget = 90 ;
+let price  = 40 ;
+if (budget <= price) {
+    console.log ("buy");
+  }  else {
+    console.log ("dont buy");
+  }
