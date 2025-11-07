11553daaa8339db1fe09f638495746d0148685dc
akhilmanne23-star <akhilmanne23@gmail.com>
2025-11-06 16:21:57 -0800

    inital comit

---
+++ test.js
@@ -0,0 +1,34 @@
+// // Example: "I'll study at the libarary if it's open and if i dont have any chores at home."
+
+// // AND
+// // Two varaibles: libarayOpen, noChores
+
+// let libraryOpen = true;
+// let noChores = false;
+
+// if (libraryOpen && noChores) {
+
+// console.log("i will study at the library");
+// } else{
+// console.log ("The library is closed and i have chores to do, i dont have any chores but the libaray is closed, Libaray is open but i have chores");
+// }
+
+
+// let budget = 50;
+// let price = 40;
+// if (budget >= price) {
+//     console.log("i will buy the item");
+// } else {
+//     console.log("i cant buy the item");
+// }
+
+
+let x = 80;
+let y = 100;
+if (x >= y) {
+    console.log("i will buy the item");
+} else {
+    console.log("i cant buy the item");
+}
+
+l
