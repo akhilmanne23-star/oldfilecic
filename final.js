6c8452925b8e618a75dbc44d7e943b9db7ae0b49
Akhil Manne <akhilmanne23@gmail.com>
2025-11-13 17:26:14 -0800

    whileloop

--- final.js
+++ final.js
@@ -1,1 +1,22 @@
-console.log ("world");
+// let x = 10 ;
+// let y = 20 ;
+// let Z =  x +y ;
+// console.log ("New message");
+
+// // // counting up from 1 to 10
+// for(let x = 1 ; x <= 10 ; x++){
+//  console.log (`count up: ${x}`);
+//  console.log ("count up:", x);
+// }
+   
+
+// // counting up from 10 to 
+for(let x =10 ; x>=1; x--){
+    console.log (`countdown : ${x}`);
+}
+  
+// count up from 1 to 20 , but only print odd number
+
+for(let y =1 ;y <=20; y+=2){
+  console.log (`count up:${y}`);
+}
---
+++ lec9.js
@@ -0,0 +1,10 @@
+// infinate loop
+// let x =1;
+// while (true){
+//     console.log ("infinate loop:", x)
+//     x++;
+// }
+
+for (let y = 1; y <= 10 ; y += 2){
+    console.log (y);
+}
